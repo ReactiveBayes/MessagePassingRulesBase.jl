0c47744092a135472496421166c48834bbe25442
Documenter.jl <documenter@juliadocs.github.io>
2025-07-28 10:26:43 +0000

    build based on bfa6908

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MessagePassingRulesBase","page":"Home","title":"MessagePassingRulesBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MessagePassingRulesBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
